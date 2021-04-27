343f89050d8504debc0434c84dd92c42e2c31543
Sam Smith <sam@example.com>
2021-04-26 21:34:18 -0400

    added new subnet

--- east/db.tf
+++ east/db.tf
@@ -9,7 +9,7 @@ data "aws_subnet_ids" "default" {
 
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = [data.aws_subnet_ids.default.ids[0], data.aws_subnet_ids.default.ids[1]]
+  subnet_ids = [data.aws_subnet_ids.default.*.id]
 }
 
 resource "aws_db_instance" "default" {
