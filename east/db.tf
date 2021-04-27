f9a1437341192a6763f5cab2db44502cf8d55937
Sam Smith <sam@example.com>
2021-04-26 21:16:44 -0400

    added new subnet

--- east/db.tf
+++ east/db.tf
@@ -21,7 +21,7 @@ resource "aws_db_instance" "default" {
   name                 = "mydb"
   username             = "foo"
   password             = var.password
-  parameter_group_name = "default-mysql5.7"
+  parameter_group_name = "default-mysql57"
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.default.name
   vpc_security_group_ids = aws_security_group.default.id
@@ -29,7 +29,7 @@ resource "aws_db_instance" "default" {
 }
 
 resource "aws_db_parameter_group" "default" {
-  name        = "default-mysql5.7"
+  name        = "default-mysql57"
   family      = "mysql5.7"
   parameter {
     name  = "character_set_server"
